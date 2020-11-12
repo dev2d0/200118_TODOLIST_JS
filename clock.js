8d42555630ea56fed4f96f07308dd5e03a87cc33
이동영 <leedy2491@ajou.ac.kr>
2020-11-12 19:02:06 +0900

    completed

--- clock.js
+++ clock.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-const clockTitle = document.querySelector(".clock_title");
-
-function getTime() {
-  const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-
-  clockTitle.innerText = `${
-    hours>12?`PM ${hours-12}`:`AM ${hours}`
-  }:${
-    minutes<10?`0${minutes}`:`${minutes}`
-  }:${
-    seconds<10?`0${seconds}`:`${seconds}`
-  }`;
-}
-
-function init() {
-  getTime();
-  setInterval(getTime, 1000);
-}
-
-=======
 const clockTitle = document.querySelector(".clock_title");
 
 function getTime() {
@@ -44,5 +20,4 @@ function init() {
     setInterval(getTime, 1000);
 }
 
->>>>>>> a5ef4303ea5c17006f59fd3395a2e0a1723cc6fd
 init();
